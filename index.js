ac0a94415ac6608bb69e127ed889c2fb47a42ce3
rogeregrge <rogervidal94@gmail.com>
2025-04-30 16:34:12 -0300

    index test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TO
 // Rota básica para teste
 app.get('/', (req, res) => {
     console.log('Recebido GET /');
-    res.status(200).send('Tudo funcionando');
+    res.status(200).send('Tudo funcionandoooooo');
   });
 
 // Rota de envio de mensagem
